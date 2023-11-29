256181f25b6e3e092c12fc7637c02f7656b9f42c
HenkSeed <adaner@inbox.ru>
2023-11-29 23:27:12 +0700

    First Commit

---
+++ Test/seminars/third/coverage/SomeServiceTest.java
@@ -0,0 +1,43 @@
+package seminars.third.coverage;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SomeServiceTest {
+   // 3.1.
+
+    private SomeService someService;
+
+    @BeforeEach
+    void setUp(){ someService = new SomeService(); }
+
+    @Test
+    void testReturnFizz(){
+        assertThat(someService.fizzBuzz(3)).isEqualTo("Fizz");
+    }
+
+    @Test
+    void testReturnBuzz(){
+        assertThat(someService.fizzBuzz(5)).isEqualTo("Buzz");
+    }
+
+    @Test
+    void  testReturnFizzBuzz(){
+        assertThat(someService.fizzBuzz(15)).isEqualTo("FizzBuzz");
+    }
+
+    @Test
+    void testReturnValueString(){
+        assertThat(someService.fizzBuzz(11)).isEqualTo("11");
+    }
+
+    void multipleThreeNotFiveReturnsFizz(int n) {
+        // assertEquals...
+    }
+
+}
