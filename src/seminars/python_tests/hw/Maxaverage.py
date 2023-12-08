b94153b375bff2aa18a7c5f33141cc51a0bd1152
HenkSeed <adaner@inbox.ru>
2023-12-08 23:21:28 +0700

    Added Seminar6 HomeWork (Python)

---
+++ src/seminars/python_tests/hw/Maxaverage.py
@@ -0,0 +1,18 @@
+class Maxaverage:
+    @staticmethod
+    def find_average(numbers):
+        if not isinstance(numbers, list):
+            raise TypeError("Input should be a list")
+        if not numbers:
+            return 0
+        return sum(numbers) / len(numbers)
+    @staticmethod
+    def find_max(a, b):
+        if a > b:
+            result = "Первый список имеет большее среднее значение"
+        elif a == b:
+            result = "Средние значения равны"
+        else:
+            result = "Второй список имеет большее среднее значение"
+        print("\n" + result)
+        return result
---
+++ src/seminars/python_tests/hw/Test_max_average.py
@@ -0,0 +1,38 @@
+# import unittest
+# import pytest
+
+from Maxaverage import Maxaverage
+
+
+def test_find_average():
+    numbers = [10, 20, 30, 40, 50]
+    assert Maxaverage.find_average(numbers) == 30
+
+
+def test_average_zero():
+    # numbers = []
+    assert Maxaverage.find_average([]) == 0
+
+def test_average_alone():
+    # numbers = [7]
+    assert Maxaverage.find_average([7]) == 7
+
+
+def test_find_max_first():
+    assert Maxaverage.find_max(30, 25) == "Первый список имеет большее среднее значение"
+
+
+def test_find_max_second():
+    assert Maxaverage.find_max(25, 30) == "Второй список имеет большее среднее значение"
+
+
+def test_find_both_equal():
+    assert Maxaverage.find_max(30, 30) == "Средние значения равны"
+
+
+def test_find_max_second_integrated():
+    numbers1 = [10, 20, 30, 40, 50]
+    numbers2 = [15, 25, 35, 45, 55]
+    assert (Maxaverage.find_max(Maxaverage.find_average(numbers1),
+                                Maxaverage.find_average(numbers2)) ==
+            "Второй список имеет большее среднее значение")
